9754a282737175b73000b5ce3a9b534901c232ff
mousavir <mousavir@educ.college_montreal.qc.ca>
2022-09-26 15:59:15 -0400

    commit final

--- word_count.py
+++ word_count.py
@@ -1,9 +1,16 @@
 #Rozhina Mousavi
 #13 septembre 2022
 #TP1 word_count
-chaine = (input("Écriver une chaine de caractères:")) #variable chaine est egale au chaine au caractères qu'entre l"utilisateur
 
-def count_word(): #fonction count_word() qui calcule et donne le nombre de mots dans le chaine de caracteres qu'a entre l'utilisateur
-   print("Il y a " + str(len(chaine.split(" "))) + " mots dans cette chaine de caractères")
+#variable chaine est egale au chaine au caractères qu'entre l"utilisateur
+chaine = (input("Écriver une chaine de caractères:"))
 
-count_word() #appel au fonction count_word()
+#fonction count_word() qui recoit l'input de l'utilisateur calcule et retourne le nombre de mots dans le chaine de caracteres qu'a entre l'utilisateur
+def count_word(chaine):
+   return(len(chaine.split(" ")))
+
+#variable nombre_de_mots est égale à la fonction count_word
+nombre_de_mots = count_word(chaine) #appel au fonction count_word()
+
+#affichage à l'ecran du nombre de mots dans la chaine de caractère grace à l'appel au fonction count_word
+print("Il y a " + str(nombre_de_mots) + " mots dans cette chaine de caractères")
