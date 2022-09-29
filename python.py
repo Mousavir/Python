c1d68cc898cdf4d47e236f46cfd840e00437d3de
mousavir <mousavir@educ.college-montreal.qcc.a>
2022-09-29 10:43:08 -0400

    commit initial

---
+++ python.py
@@ -0,0 +1,9 @@
+import random
+borne_minimal= (int(input("Choisissez le nombre minimal pour la borne de nombre aléatoire")))
+borne_maximal = (int(input("Choisissez le nombre maximal pour la borne de nombre aléatoire")))
+
+x = random.randint((borne_minimal, borne_maximal))
+
+print(borne_minimal)
+print(borne_minimal)
+print(x)
