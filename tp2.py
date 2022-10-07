807311ee309f95254946453874d560d559ae3e63
mousavir <mousavir@educ.college_montreal.qc.ca>
2022-10-07 15:56:47 -0400

    commit

--- tp2.py
+++ tp2.py
@@ -1,9 +1,10 @@
 import random
-borne_minimal = (int(input("Choisissez le nombre minimal pour la borne de nombre aléatoire")))
-borne_maximal = (int(input("Choisissez le nombre maximal pour la borne de nombre aléatoire")))
+borne_minimal = ((input("Choisissez le nombre minimal pour la borne de nombre aléatoire:")))
+borne_maximal = ((input("Choisissez le nombre maximal pour la borne de nombre aléatoire:")))
+
+print(borne_minimal)
+print(borne_maximal)
 
 x = random.randint((borne_minimal, borne_maximal))
 
-print(borne_minimal)
-print(borne_minimal)
-print(x)
+
