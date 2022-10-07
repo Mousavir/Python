189f735c241c406c52495dc1eb831a84c1f0dd75
mousavir <mousavir@educ.college_montreal.qc.ca>
2022-10-07 16:28:01 -0400

    commit 5

--- word_count.py
+++ word_count.py
@@ -4,10 +4,10 @@
 
 #variable chaine est egale au chaine au caractères qu'entre l"utilisateur
 chaine = (input("Écriver une chaine de caractères:"))
-
+nombre_dans_chaine = len(chaine.split(" "))
 #fonction count_word() qui recoit l'input de l'utilisateur calcule et retourne le nombre de mots dans le chaine de caracteres qu'a entre l'utilisateur
 def count_word(chaine):
-   return(len(chaine.split(" ")))
+   return(nombre_dans_chaine)
 
 #variable nombre_de_mots est égale à la fonction count_word
 nombre_de_mots = count_word(chaine) #appel au fonction count_word()
