187eb4f9b30cc4d358f1d20a21c284fc6b3d6aff
mousavir <mousavir@educ.college-montreal.qc.ca>
2022-10-08 19:44:38 -0400

    commit

--- tp2.py
+++ tp2.py
@@ -1,10 +1,19 @@
 import random
 
-def
-borne_minimal = (int(input("Choisissez le nombre minimal pour la borne de nombre aléatoire:")))
-borne_maximal = (int(input("Choisissez le nombre maximal pour la borne de nombre aléatoire:")))
+choisis = (input("Voulez vous choisir votre propre borne pour le nombre ou préférer vous l'option par défaut (0-100)? Répondez choisis ou défaut:"))
+def choisis_bornes(choisis):
 
-print(borne_minimal)
-print(borne_maximal)
+    if choisis == "choisis":
+        borne_minimal = (int(input("Choisissez le nombre minimal pour la borne de nombre aléatoire:")))
+        borne_maximal = (int(input("Choisissez le nombre maximal pour la borne de nombre aléatoire:")))
 
-x = random.randint(borne_minimal, borne_maximal)
+
+        print(borne_minimal)
+        print(borne_maximal)
+
+        x = random.randint(borne_minimal, borne_maximal)
+
+    elif choisis == "défaut":
+        x = random.randint(0, 100)
+
+choisis_bornes(choisis)
