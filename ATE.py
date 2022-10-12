227ef28e4eced0eab108491d3c731ec166eba5c4
mousavir <mousavir@educ.college-montreal.qcc.a>
2022-10-12 09:20:01 -0400

    commit initial

--- ATE.py
+++ ATE.py
@@ -22,39 +22,39 @@ def jeu():
     print( x)
     print("J'ai choisi un nombre au hasard entre " + str(borne_minimal) + " et " + str(borne_maximal) + ". À vous de deviner...")
     return x
+    while boucle_essais:
+        global x
+        nb = jeu()
+        essai = (int(input("Entrez votre essai:")))
+        print(str(essai))
+
+        if essai < nb:
+            print("x >", (int(essai)))
+            essais()
+
+
+        elif essai > nb:
+            print("x <", (int(essai)))
+            essais()
+
+        elif essai == nb:
+            print("Bravo! Bonne réponse!")
+            essais()
+
+            print("Vous avez réussi en " + str(nombres_essai) + " essai(s)!")
+            quitter = (input("Voulez-vous faire une autre partie (o/n)?:"))
+            print(quitter)
+            if quitter == "o":
+                boucle_essais = True
+
+
+
+            elif quitter == "n":
+                print("Merci et aurevoir...")
+                boucle_essais = False
+
 
 while boucle_jeu:
     jeu()
-    boucle_jeu=False
-
-while boucle_essais:
-    global x
-    nb= jeu()
-    essai = (int(input("Entrez votre essai:")))
-    print(str(essai))
-
-    if essai < nb:
-        print("x >", (int(essai)))
-        essais()
 
 
-    elif essai > nb:
-        print("x <", (int(essai)))
-        essais()
-
-    elif essai == nb:
-        print("Bravo! Bonne réponse!")
-        essais()
-
-        print("Vous avez réussi en " + str(nombres_essai) + " essai(s)!")
-        quitter = (input("Voulez-vous faire une autre partie (o/n)?:"))
-        print(quitter)
-        if quitter == "o":
-            boucle_essais = True
-            boucle_jeu = True
-
-
-        elif quitter == "n":
-            print("Merci et aurevoir...")
-            boucle_essais =False
-            boucle_jeu = False
