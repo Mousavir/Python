0f8bb6d298ce059d6dacfb1c91a4b04dc691c32b
mousavir <mousavir@educ.college-montreal.qcc.a>
2022-10-25 09:18:21 -0400

    commit initial

--- TP3.py
+++ TP3.py
@@ -1,7 +1,9 @@
 import random
 
 niveau_vie = 20
-force_adversaire = random.randint(1,5)
+force_adversaire = random.randint(1, 5)
+boucle_jeu = True
+
 
 print("Voux tombez face à face avec un adversaire de difficulté:" + str(force_adversaire))
 
@@ -11,17 +13,38 @@ quoi_faire = input("""Que voulez-vous faire?
 3- Afficher les règles du jeu
 4- Quitter la partie
 *Entrer le numero de l'option choisis*
-Réponse:""")
+Décision:""")
 
 print(quoi_faire)
 
 if  quoi_faire == "1":
-    print("ok")
+    lance_dé = random.randint(1,6)
+    if lance_dé <= force_adversaire:
+
 
 elif quoi_faire == "2":
-    print("oui")
+    niveau_vie -= 1
+    print("Vous recevez une pénalité de 1 point de vie. Voici votre niveau de vie mise à jour:" + str(niveau_vie))
+
 elif quoi_faire == "3":
-    print("non")
+    print("""Pour réussir un combat, il faut que la valeur du dé lancé soit supérieure à la force de l’adversaire. 
+Dans ce cas, le niveau de vie de l’usager est augmenté de la force de l’adversaire.
+Une défaite a lieu lorsque la valeur du dé lancé par l’usager est inférieure ou égale à la force de l’adversaire.  Dans ce cas, le niveau de vie de l’usager est diminué de la force de l’adversaire.
+La partie se termine lorsque les points de vie de l’usager tombent sous 0.
+L’usager peut combattre ou éviter chaque adversaire, dans le cas de l’évitement, il y a une pénalité de 1 point de vie. """)
 
 elif quoi_faire == "4":
-    print()
+    print("Merci et aurevoir!")
+
+
+
+
+
+
+
+
+
+
+
+
+
