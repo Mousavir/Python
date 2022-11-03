c7e61d9428560bfa53d5f157780994b50340c329
mousavir <mousavir@educ.colege-montreal.qc.ca>
2022-11-03 16:17:02 -0400

    commit

--- tests.py
+++ tests.py
@@ -1,12 +1,18 @@
 import random
 niveau_vie = 20
 force_adversaire = random.randint(1, 5)
-numero_adversaire = random.randint(1,100)
+numero_adversaire = random.randint(0,100)
 numero_combat = 0
 nombres_de_victoires = 0
 nombres_de_defaites = 0
 decision = "1"
 
+def combat():
+    global numero_combat
+    numero_combat +=1
+
+def nb_victoires_consécutifs():
+    nombres_de_victoire
 def new_niveau_vie():
     global niveau_vie
     niveau_vie += int(force_adversaire)
@@ -22,6 +28,7 @@ def statut_partie():
     print("Combat " + str(numero_combat) + ": "+ str(nombres_de_victoires) + " victoires vs " +str(nombres_de_defaites) + " defaites")
 
 if decision == "1":
+    combat()
     statut_partie()
     score_dé = random.randint(1, 6)
     print("Lancé du dé:" + str(score_dé))
@@ -40,4 +47,5 @@ if decision == "1":
 
     print("Dernier combat = " + str(combat_statut))
     print("Niveau de vie = " + str(niveau_vie))
+
 
