e463728a3a878f708f5fa87e4fd41947cceedd17
mousavir <mousavir@educ.college-montreal.qcc.a>
2022-12-07 09:17:32 -0500

    commit initial

---
+++ PPO.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+
+class Person:
+    def __init__(self):
+        self.name = ''
+
+class Student(Person):
+    def __init__(self):
+        super().__init__()
+
+        self.id = 0
+
+audrey = Person()
+audrey.name = 'Audrey'
+
+eleve_1 = Student()
+eleve_1.name = 'Jean'
+eleve_1.id = 123456
