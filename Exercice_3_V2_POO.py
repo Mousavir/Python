067802547c8e66f0bfd6966ce390335982429340
mousavir <mousavir@educ.college-montreal.qc.ca>
2022-12-16 00:21:20 -0500

    commit final

---
+++ Exercice_3_V2_POO.py
@@ -0,0 +1,11 @@
+from math import pi
+
+#classe cercle qui calcul air cerlce aver valeurs de l'air cercle et son circomference avec nombre pour rayon predefinit
+class Cercle:
+    #fonction calculer air cerlce avec formule, nombre pi et valeur definit rayon cercle
+    def calcul_air(self):
+        return pi * 5 ** 2
+
+    # fonction calculer circomference cerlce avec formule, nombre pi et valeur definit rayon cercle
+    def calcul_circonference(self):
+        return 2 * pi * 5
