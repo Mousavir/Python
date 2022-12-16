ac5811bb124c47eb046d160e5e7da7a97be9b921
mousavir <mousavir@educ.college-montreal.qc.ca>
2022-12-16 15:17:44 -0500

    commit

--- Exercice_3_V1_POO.py
+++ Exercice_3_V1_POO.py
@@ -2,7 +2,7 @@ from math import pi
 
 #classe cercle qui recoit valeur rayon en parametre et qui avec cette information + pi calcule l'air et la circonference du cercle
 class Cercle:
-    #fonction pour creru objet cercle avec rayon variale qui est associé a une valeur qui sera définit par la suite
+    #fonction pour creer objet cercle avec rayon variale qui est associé a une valeur qui sera définit par la suite
     def __init__(self, parametre_1):
         self.rayon = parametre_1
     #fonction pour calculer l'air du cercle en utilisa vairbale rayon, le nombre pi et formule
