65be2e46ac1a27e675b71d3148cdd87a55591aa5
mousavir <mousavir@educ.college-montreal.qc.ca>
2022-12-16 00:08:17 -0500

    commit final

--- Exercice_1_POO.py
+++ Exercice_1_POO.py
@@ -1,4 +1,4 @@
-#classe stringfoo qui contient informations pour accepter un string et le sauvgarder dans un attribut puis qui a la possilibité de l'inprimer
+#classe stringfoo qui contient informations pour accepter un string et le sauvgarder dans un attribut puis qui a la possilibité de l'imprimer
 class StringFoo:
     #fonction qui contient informations sur la variable name qui pour l'instat est vide (objet créer)
     def __init__(self):
