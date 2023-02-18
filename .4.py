d09956d875ad33ec2dc46aef15c558bbf2d63693
mousavir <mousavir@educ.college-montreal.qc.ca>
2023-02-18 00:17:08 -0500

    commit

---
+++ .4.py
@@ -0,0 +1,18 @@
+import arcade
+
+
+class MyGame(arcade.Window):
+   def __init__(self, width, height, title):
+       # Call the parent class's init function
+       super().__init__(width, height, title)
+
+
+def main():
+
+   window = MyGame(640, 480, "Drawing Example")
+   arcade.run()
+   arcade.start_render()
+
+   arcade.draw_circle_filled(10, 10, 20, (255, 54, 34))
+
+main()
