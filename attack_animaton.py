b1225fda21a9e73c3b59816a888d9327e6c60e81
mousavir <mousavir@educ.college-montreal.qcc.a>
2023-04-19 10:31:11 -0400

    commit initial

---
+++ attack_animaton.py
@@ -0,0 +1,42 @@
+from enum import Enum
+
+class AttackType(Enum):
+
+   ROCK = 0
+   PAPER = 1
+   SCISSORS = 2
+
+class AttackAnimation(arcade.Sprite):
+   ATTACK_SCALE = 0.50
+   ANIMATION_SPEED = 5.0
+
+self.rock = AttackAnimation(AttackType.ROCK)
+self.paper = AttackAnimation(AttackType.PAPER)
+self.scissors = AttackAnimation(AttackType.SCISSORS)
+
+def __init__(self, attack_type):
+   super().__init__()
+
+   self.attack_type = attack_type
+   if self.attack_type == AttackType.ROCK:
+       self.textures = [
+           arcade.load_texture("assets/srock.png"),
+           arcade.load_texture("assets/srock-attack.png"),
+       ]
+   elif self.attack_type == AttackType.PAPER:
+       self.textures = [
+           arcade.load_texture("assets/spaper.png"),
+           arcade.load_texture("assets/spaper-attack.png"),
+       ]
+   else:
+       self.textures = [
+           arcade.load_texture("assets/scissors.png"),
+           arcade.load_texture("assets/scissors-close.png"),
+       ]
+
+   self.scale = self.ATTACK_SCALE
+   self.current_texture = 0
+   self.set_texture(self.current_texture)
+
+
+
---
+++ game_state.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+class GameSate(Enum):
+    NOT_STARTED
+
+    ROUND_ACTIVE
+    ROOUND_DONE
+    GAME_OVER
+
